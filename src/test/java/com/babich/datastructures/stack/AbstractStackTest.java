12fe73929c7a02661ebaa42ec15274f233eafed7
IrinaBabich <Iringjob@gmail.com>
2018-05-06 09:50:07 +0300

    tenth commit

--- src/test/java/com/babich/datastructures/stack/AbstractStackTest.java
+++ src/test/java/com/babich/datastructures/stack/AbstractStackTest.java
@@ -3,8 +3,6 @@ package com.babich.datastructures.stack;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.util.NoSuchElementException;
-
 import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -34,37 +32,37 @@ public abstract class AbstractStackTest {
         stackWithZeroElements.push("C");
         stackWithZeroElements.push("D");
 
-        assertEquals("D", stackWithZeroElements.peek());
-        assertEquals("C", stackWithZeroElements.peek());
-        assertEquals("B", stackWithZeroElements.peek());
-        assertEquals("A", stackWithZeroElements.peek());
+        assertEquals("D", stackWithZeroElements.pop());
+        assertEquals("C", stackWithZeroElements.pop());
+        assertEquals("B", stackWithZeroElements.pop());
+        assertEquals("A", stackWithZeroElements.pop());
     }
 
     @Test
     public void testPeek() {
         stackWithZeroElements.push("A");
+        stackWithZeroElements.push("B");
+        stackWithZeroElements.push("C");
+        stackWithZeroElements.push("D");
+
+        assertEquals("D", stackWithZeroElements.peek());
+        assertEquals("C", stackWithZeroElements.peek());
+        assertEquals("B", stackWithZeroElements.peek());
         assertEquals("A", stackWithZeroElements.peek());
-        assertEquals("E", stackWithFiveElements.peek());
-        stackWithFiveElements.pop();
-        assertEquals("D", stackWithFiveElements.peek());
-        stackWithFiveElements.push(1);
-        stackWithFiveElements.push(2);
-        assertEquals(2, stackWithFiveElements.peek());
+
         assertTrue(stackWithFiveElements.peek().equals(2));
         assertFalse(stackWithFiveElements.peek().equals("2"));
     }
 
     @Test
     public void testPop() {
-        stackWithZeroElements.push("A");
-        stackWithZeroElements.push("B");
-        stackWithZeroElements.push("C");
-
+        assertEquals(5, stackWithFiveElements.size());
         assertEquals("E", stackWithFiveElements.pop());
         assertEquals("D", stackWithFiveElements.pop());
         assertEquals("C", stackWithFiveElements.pop());
         assertEquals("B", stackWithFiveElements.pop());
         assertEquals("A", stackWithFiveElements.pop());
+
         assertTrue(stackWithFiveElements.size() == 0);
         assertEquals("C", stackWithZeroElements.pop());
         assertEquals("B", stackWithZeroElements.pop());
